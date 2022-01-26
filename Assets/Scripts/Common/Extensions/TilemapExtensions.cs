f2f5a606c9d26e3c3bf00859f3439a04536ee6b6
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2022-01-26 14:23:40 +0100

    Replace drawing line with closed tiles when reaches the close tile.

---
+++ Assets/Scripts/Common/Extensions/TilemapExtensions.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine.Tilemaps;
+
+namespace Common.Extensions
+{
+    public static class TilemapExtensions
+    {
+        public static int CountActiveTiles(this Tilemap tilemap)
+        {
+            var count = 0;
+
+            foreach (var tilePosition in tilemap.cellBounds.allPositionsWithin)
+            {
+                if (tilemap.HasTile(tilePosition)) 
+                    count++;
+            }
+
+            return count;
+        }
+    }
+}
--- Assets/Scripts/Player/PlayerLineMovementDrawer.cs
+++ Assets/Scripts/Player/PlayerLineMovementDrawer.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
 using System.Linq;
+using Common.Extensions;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 namespace Player
 {
@@ -10,13 +12,22 @@ namespace Player
         [SerializeField] private Transform playerPosition;
         [SerializeField] private LayerMask layerToDrawLine;
         
+        [Header("Tiles")]
+        [SerializeField] private Tilemap openGameAreaTileMap;
+        [SerializeField] private Tilemap closeGameAreaTileMap;
+        [SerializeField] private Tile closeTile;
+        
         private LineRenderer _lineRenderer;
         private List<Vector3> _moveLinePoints;
+        private bool _isDrawing;
+        private int _numberOfOpenTilesAtStart;
 
         private void Awake()
         {
             _lineRenderer = GetComponent<LineRenderer>();
             _moveLinePoints = new List<Vector3>();
+            _numberOfOpenTilesAtStart = openGameAreaTileMap.CountActiveTiles();
+            Debug.Log(_numberOfOpenTilesAtStart);
         }
 
         private void Update()
@@ -33,20 +44,55 @@ namespace Player
             {
                 _moveLinePoints.Add(playerPosition.position);
                 AssignDrawingPoints();
-            }
-            else
-            {
-                // TODO: Fill area with CloseGameArea tiles
                 
+                _isDrawing = true;
+            }
+            else if (_isDrawing)
+            {
+                SwapOpenTilesToCloseTiles();
+                
+                // We can here recalculate percentage of closed tiles
+                Debug.Log(CalculatePercentageOfClosedTiles() + "%");
+
                 _moveLinePoints.Clear();
                 AssignDrawingPoints();
+                
+                _isDrawing = false;
             }
+        }
+
+        private void SwapOpenTilesToCloseTiles()
+        {
+            // TODO: Calculate all tiles to replace, not just a lines (Flood Fill Algorithm)
+            var positionsToSwap = _moveLinePoints
+                .Select(Vector3Int.FloorToInt)
+                .Distinct()
+                .ToArray();
+            
+            // SetTiles() not working... ???
+
+            foreach (var pos in positionsToSwap)
+            {
+                openGameAreaTileMap.SetTile(pos, null);
+                closeGameAreaTileMap.SetTile(pos, closeTile);
+            }
+
+            openGameAreaTileMap.CompressBounds();
+
+            // TODO: Apply Flood Fill Algorithm here
         }
 
         private void AssignDrawingPoints()
         {
             _lineRenderer.positionCount = _moveLinePoints.Count;
             _lineRenderer.SetPositions(_moveLinePoints.ToArray());
+        }
+
+        private int CalculatePercentageOfClosedTiles()
+        {
+            var currentOpenTilesCount = openGameAreaTileMap.CountActiveTiles();
+
+            return 100 - currentOpenTilesCount * 100 / _numberOfOpenTilesAtStart;
         }
     }
 }
