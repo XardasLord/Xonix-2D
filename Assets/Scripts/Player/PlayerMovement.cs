bf602533ff4d0477c2fe269620fc38c8dd4a97d4
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2022-01-27 17:11:05 +0100

    Change PlayerMovement logic

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,52 +4,62 @@ namespace Player
 {
     public class PlayerMovement : MonoBehaviour
     {
+        [Range(1f, 20f)]
         [SerializeField] private float moveSpeed = 10f;
-        [SerializeField] private Transform movePoint;
         [SerializeField] private LayerMask layerStopsMovement;
+
+        private bool _isMoving;
+        private Vector3 _targetPosition;
 
         private void Start()
         {
-            movePoint.parent = null;
+            _targetPosition = transform.position;
         }
 
         private void Update()
         {
-            MoveToMovementPosition();
-            SetNewMovementPosition();
+            if (_isMoving)
+                MoveTowardsToTargetPosition();
+            else
+                SetNewTargetPositionFromInput();
+        }
+        
+        private void MoveTowardsToTargetPosition()
+        {
+            transform.position = Vector2.MoveTowards(transform.position, _targetPosition, moveSpeed * Time.deltaTime);
+
+            if (transform.position == _targetPosition)
+                _isMoving = false;
         }
 
-        private void MoveToMovementPosition()
+        private void SetNewTargetPositionFromInput()
         {
-            transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
-        }
-
-        private void SetNewMovementPosition()
-        {
-            if (Vector3.Distance(transform.position, movePoint.position) > .05f)
-                return;
-
             var inputX = Input.GetAxisRaw("Horizontal");
             var inputY = Input.GetAxisRaw("Vertical");
 
             if (inputX != 0f)
             {
-                var movePosition = movePoint.position + new Vector3(inputX, 0f);
+                var direction = new Vector3(inputX, 0f);
 
-                if (!Physics2D.OverlapCircle(movePosition, .2f, layerStopsMovement))
+                if (CanMove(direction))
                 {
-                    movePoint.position = movePosition;
+                    _targetPosition += direction;
+                    _isMoving = true;
                 }
-            }
+            } 
             else if (inputY != 0f)
             {
-                var movePosition = movePoint.position + new Vector3(0f, inputY);
+                var direction = new Vector3(0f, inputY);
 
-                if (!Physics2D.OverlapCircle(movePosition, .2f, layerStopsMovement))
+                if (CanMove(direction))
                 {
-                    movePoint.position = movePosition;
+                    _targetPosition += direction;
+                    _isMoving = true;
                 }
             }
         }
+
+        private bool CanMove(Vector3 direction) 
+            => !Physics2D.OverlapCircle(transform.position + direction, .2f, layerStopsMovement);
     }
 }
