c7c183eda6f34bfdf278f1141702360c8e1bdad4
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2022-01-31 14:20:32 +0100

    Improve DontDestroyOnLoad mechanism for managing game state

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,3 +1,6 @@
+using System.Collections;
+using System.Linq;
+using TMPro;
 using UnityAtoms.BaseAtoms;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -7,42 +10,56 @@ namespace Managers
     public class GameManager : MonoBehaviour
     {
         [Header("Events")]
-        [SerializeField] private IntEvent levelChangedEvent; 
+        [SerializeField] private IntEvent levelChangedEvent;
+
+        [SerializeField] private TextMeshProUGUI nextLevelText;
         
-        private static GameManager instance;
+        private static GameManager _instance;
         
         private static int _levelNumber = 0;
-        private static int _numberOfEnemies = 1;
         private static int _nextLevelPercentageValue = 60;
-        
-        private int _maxNextLevelPercentageValue = 90;
-        
+
+        private const int MaxNextLevelPercentageValue = 90;
+
         private void Awake()
         {
-            DontDestroyOnLoad(this);
-            
-            if (instance == null)
-                instance = this;
-            else
-                Destroy(gameObject);
+            if (_instance == null) {
+                // First run, set the instance
+                _instance = this;
+                DontDestroyOnLoad(gameObject);
+ 
+            } else if (_instance != this) {
+                // Instance is not the same as the one we have, destroy old one, and reset to newest one
+                Destroy(_instance.gameObject);
+                _instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
             
             _levelNumber++;
             levelChangedEvent.Raise(_levelNumber);
 
             _nextLevelPercentageValue += 2;
-            if (_nextLevelPercentageValue > _maxNextLevelPercentageValue)
-                _nextLevelPercentageValue = _maxNextLevelPercentageValue;
+            if (_nextLevelPercentageValue > MaxNextLevelPercentageValue)
+                _nextLevelPercentageValue = MaxNextLevelPercentageValue;
             
-            // TODO: Spawn enemies
-            _numberOfEnemies++;
+            nextLevelText ??= FindObjectsOfType<TextMeshProUGUI>().Single(x => x.name == "NextLevelText");
+            nextLevelText.text = string.Empty;
         }
 
         public void ScoreChanged(int filledPercentage)
         {
             if (filledPercentage > _nextLevelPercentageValue)
             {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                nextLevelText.text = "Prepare for the next level...";
+                StartCoroutine(LoadLevelAfterDelay(2f));
             }
+        }
+ 
+        private IEnumerator LoadLevelAfterDelay(float delay)
+        {
+            yield return new WaitForSeconds(delay);
+            
+            SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);
         }
     }
 }
